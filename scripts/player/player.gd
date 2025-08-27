0cdb7788b8afea022194e56f97e43d81832e99d1
naexrow <till.wolf2002@gmail.com>
2025-08-27 16:30:50 +0200

    setup

---
+++ scripts/player/player.gd
@@ -0,0 +1,13 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float = 100.0
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+	#var direction: vector2 = vector2.ZERO
+	
+	
