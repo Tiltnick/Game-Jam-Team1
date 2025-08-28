0c2912761c2ed50477b1e08dd73997456f424d82
Yannick <yannickkissel@web.de>
2025-08-28 14:55:56 +0200

    Pathfinding enemy

--- scripts/enemies/slime_path.gd
+++ scripts/enemies/slime_path.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 
 const speed = 200
-@export var chest: Node2D
+@export var player: Node = null
 @onready var navAgent := $NavigationAgent2D as NavigationAgent2D
 
 func _ready() -> void:
-	makePath()
+	navAgent.target_position = player.global_position
 
 func _physics_process(_delta: float) -> void:
-	var dir = to_local(navAgent.get_next_path_position()).normalized()
-	velocity = dir * speed
-	move_and_slide()
-	
-func makePath() -> void:
-	navAgent.target_position = chest.global_position
-	
+	if !navAgent.is_target_reached():
+		var navPointDirection = to_local(navAgent.get_next_path_position()).normalized()
+		velocity = navPointDirection * speed
+		move_and_slide()
+		
+		
 func _on_timer_timeout():
-	makePath()
+	if navAgent.target_position != player.global_position:
+		navAgent.target_position = player.global_position
+	$Timer.start()
