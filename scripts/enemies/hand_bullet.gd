0528ffddc36a78d88e9a43dd4f552a906208792a
naexrow <till.wolf2002@gmail.com>
2025-08-29 22:45:23 +0200

    added upper player hitbox

--- scripts/enemies/hand_bullet.gd
+++ scripts/enemies/hand_bullet.gd
@@ -78,3 +78,8 @@ func _on_body_entered(body: Node) -> void:
 
 func _on_timeout() -> void:
 	queue_free()
+
+func _on_area_entered(area: Area2D):
+	if area.is_in_group("player") and area.has_method("take_damage"):
+		area.take_damage(damage)
+		queue_free()
---
+++ scripts/player/upperhitbox.gd
@@ -0,0 +1,4 @@
+class_name UpperHitbox extends Area2D
+
+func take_damage(dmg):
+	get_parent().take_damage(dmg)
