b88e8637bcaab335e319c0a5740f1a0b71a0caf6
naexrow <till.wolf2002@gmail.com>
2025-08-29 09:11:32 +0200

    Changed Collision disabling method

--- scripts/projectiles/ice.gd
+++ scripts/projectiles/ice.gd
@@ -21,7 +21,7 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		body.takeDamage(damage)
-		$CollisionShape2D.disabled = true
+		$CollisionShape2D.set_deferred("disabled", true)
 		speed = 0.0
 		sprite.play("hidden")
 		ani.play("hit")
